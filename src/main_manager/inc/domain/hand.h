4f5f45e326c5cd471418498b4185a95f47c1e239
take711 <bokutakepon@gmail.com>
2025-11-22 18:15:08 +0900

    add_hand.h

---
+++ src/main_manager/inc/domain/hand.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_MANAGER_INC_DOMAIN_HAND_H
+#define MAIN_MANAGER_INC_DOMAIN_HAND_H
+
+#include <stdint>
+
+namespace domain
+{
+    enum class Hand : uint8_t
+    {
+
+    }
+}
+
+#endif // MAIN_MANAGER_INC_DOMAIN_HAND_H
