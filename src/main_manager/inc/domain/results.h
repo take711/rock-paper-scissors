e6a92899d2c3eaefc29501a5dcf7608c96c687af
take711 <bokutakepon@gmail.com>
2025-11-23 12:33:24 +0900

    add_results_class

---
+++ src/main_manager/inc/domain/results.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_MANAGER_INC_DOMAIN_RESULTS_H
+#define MAIN_MANAGER_INC_DOMAIN_RESULTS_H
+
+#include <stdint.h>
+
+namespace domain {
+
+enum class results : uint8_t { win = 0, lose, draw };
+
+}
+
+#endif  // MAIN_MANAGER_INC_DOMAIN_RESULTS_H
