503fffdd73d357bfda7636824567a9ecda42cefb
take711 <bokutakepon@gmail.com>
2025-11-23 12:20:07 +0900

    add_hand_class

--- src/main_manager/inc/domain/hand.h
+++ src/main_manager/inc/domain/hand.h
@@ -1,14 +1,12 @@
 #ifndef MAIN_MANAGER_INC_DOMAIN_HAND_H
 #define MAIN_MANAGER_INC_DOMAIN_HAND_H
 
-#include <stdint>
+#include <stdint.h>
 
-namespace domain
-{
-    enum class Hand : uint8_t
-    {
+namespace domain {
 
-    }
+enum class Hand : uint8_t { rock = 0, paper, scissors };
+
 }
 
-#endif // MAIN_MANAGER_INC_DOMAIN_HAND_H
+#endif  // MAIN_MANAGER_INC_DOMAIN_HAND_H
